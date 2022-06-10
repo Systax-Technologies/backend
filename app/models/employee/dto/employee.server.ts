83d85fcccdf94cc0e4a9785dd0a2c2ca0732b786
Andrea <andrea@yaaaw.it>
2022-06-10 15:42:50 +0200

    refactor(database models): refactor database

--- app/models/employee/dto/employee.server.ts
+++
@@ -1,7 +0,0 @@
-export type Employee = {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-};
--- app/models/product/index.server.ts
+++
@@ -1,3 +0,0 @@
-export * from "./dto/product.input.server";
-export * from "./dto/product.server";
-export * from "./product.server";
