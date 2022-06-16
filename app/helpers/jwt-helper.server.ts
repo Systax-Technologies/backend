7f876024ae7e433a5e69a1283f5a6a97000d628d
BuildTools <unconfigured@null.spigotmc.org>
2022-06-16 10:17:46 +0200

    Revert "Create and verify JWT functions"
    This reverts commit f5bb3ea9a616957abfb52560119cf5295f255208.

--- app/helpers/jwt-helper.server.ts
+++ app/helpers/jwt-helper.server.ts
@@ -1,24 +1,30 @@
-import * as jwtLib from "jsonwebtoken";
+import * as crypto from "crypto";
 
-const JWT_SECRET = process.env.JWT_SECRET;
-if (JWT_SECRET === undefined) {
-  throw Error("Environment variable JWT_SECRET is not set!");
-}
+export const verify = async (jwt: string) => {
+  const verifyFunction = crypto.createVerify("RSA-SHA256");
+  var clearJwt = Buffer.from(jwt, "base64").toString("ascii");
 
-export const verifyJwt = (jwt: string) => {
-  var decoded;
-  try {
-    decoded = jwtLib.verify(jwt, JWT_SECRET);
-  } catch (err) {
-    return false;
+  const pieces = clearJwt.split(".");
+
+  // We only need the first two pieces to verify
+  verifyFunction.write(pieces[0] + "." + pieces[1]);
+  verifyFunction.end();
+  // IMPORTANT: NodeJS expects base64 format, not base64url format!
+  const jwtSignatureBase64 = Buffer.from(pieces[2]).toString("base64");
+
+  // IMPORTANT: You need to specify that the `jwtSignatureBase64` data is base64 format,
+  // otherwise, it will default to Buffer format and return false
+  if (
+    !verifyFunction.verify(
+      require("process").env.JWT_SECRET,
+      jwtSignatureBase64,
+      "base64"
+    )
+  ) {
+    throw new Response(null, {
+      status: 401,
+      statusText: "Unauthenticated",
+    });
   }
-  return decoded.toString();
-};
-
-export const createJwt = (payload: object) => {
-  payload = payload || {};
-  return jwtLib.sign(JSON.stringify(payload), JWT_SECRET, {
-    expiresIn: "1h",
-    algorithm: "HS512",
-  });
+  return;
 };
