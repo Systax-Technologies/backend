1b26d8c6636d5cb9a84503bbf4536a0051e36023
Andrea <andrea@yaaaw.it>
2022-06-23 14:58:48 +0200

    refactor: code refactor

---
+++ app/routes/api/v1/warehouse/employee/index.ts
@@ -0,0 +1,40 @@
+import { json, LoaderFunction } from "@remix-run/node";
+import { z } from "zod";
+import { verifyRequest } from "~/lib/verify-request.server";
+import { findEmployeeById } from "~/models/employee/employee.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  if (request.method.toLowerCase() !== "get") {
+    throw new Response(null, {
+      status: 405,
+      statusText: "Method Not Allowed",
+    });
+  }
+
+  const jwt = verifyRequest(request);
+
+  const schema = z.object({
+    id: z.string(),
+  });
+  const parsedData = schema.safeParse(
+    typeof jwt === "string" ? JSON.parse(jwt) : jwt,
+  );
+
+  if (parsedData.success) {
+    const employee = await findEmployeeById(parsedData.data.id);
+
+    if (employee == null) {
+      throw new Response(null, {
+        status: 404,
+        statusText: "Employee Not Found",
+      });
+    }
+
+    return json(employee);
+  } else {
+    throw new Response(null, {
+      status: 400,
+      statusText: "Invalid Request",
+    });
+  }
+};
