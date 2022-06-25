848a8117435ee29f9c6945c367948f73bfbb9692
Denis Petrin <denis.petrin@yoroi.company>
2022-06-25 13:53:31 +0200

    feat(api): add patch and delete request handler through id as parameter

--- app/routes/api/v1/warehouse/orders/by-id/$orderId.ts
+++ app/routes/api/v1/warehouse/orders/by-id/$orderId.ts
@@ -1,6 +1,13 @@
-import { Order } from "@prisma/client";
-import { LoaderFunction } from "@remix-run/node";
-import { findOrderById } from "~/models/order/order.server";
+import { Order, OrderStatus } from "@prisma/client";
+import { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { z } from "zod";
+import { methodNotAllowed } from "~/helpers/app-helpers.server";
+import { parseBody } from "~/lib/parse-body.server";
+import {
+  deleteOrder,
+  findOrderById,
+  updateOrder,
+} from "~/models/order/order.server";
 
 type LoaderData = Order;
 
@@ -12,7 +19,7 @@ export const loader: LoaderFunction = async ({
   if (!orderId) {
     throw new Response(null, {
       status: 400,
-      statusText: "Order Id Not Provided",
+      statusText: "Bad Request",
     });
   }
 
@@ -26,4 +33,77 @@ export const loader: LoaderFunction = async ({
   }
 
   return order;
+};
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const orderId = params.orderId;
+
+  if (!orderId) {
+    throw new Response(null, {
+      status: 400,
+      statusText: "Bad Request",
+    });
+  }
+
+  switch (request.method.toLowerCase()) {
+    case "patch": {
+      handlePATCHRequest(orderId, request);
+    }
+
+    case "delete": {
+      handleDELETERequest(orderId);
+    }
+
+    default: {
+      methodNotAllowed();
+    }
+  }
+};
+
+const handlePATCHRequest = async (id: string, request: Request) => {
+  const schema = z.object({
+    status: z.nativeEnum(OrderStatus),
+    orderedAt: z.date(),
+    shippedAt: z.nullable(z.date()),
+    deliveredAt: z.nullable(z.date()),
+    customerId: z.string().cuid(),
+  });
+
+  const parsedData = await parseBody(request, schema);
+
+  if (parsedData.success) {
+    const data = parsedData.data;
+    const updatedOrder = await updateOrder(id, {
+      status: data.status,
+      orderedAt: data.orderedAt,
+      shippedAt: data.shippedAt,
+      deliveredAt: data.deliveredAt,
+      customerId: data.customerId,
+    });
+    throw new Response(JSON.stringify(updatedOrder), {
+      status: 200,
+      statusText: "OK",
+    });
+  } else {
+    throw new Response(null, {
+      status: 400,
+      statusText: "Bad Request",
+    });
+  }
+};
+
+const handleDELETERequest = async (id: string) => {
+  const deletedOrder = await deleteOrder(id);
+
+  if (!deletedOrder) {
+    throw new Response(JSON.stringify(deletedOrder), {
+      status: 200,
+      statusText: "OK",
+    });
+  } else {
+    throw new Response(null, {
+      status: 400,
+      statusText: "Bad Request",
+    });
+  }
 };
--- app/routes/api/v1/warehouse/orders/index.ts
+++ app/routes/api/v1/warehouse/orders/index.ts
@@ -1,13 +1,9 @@
-import { Order, OrderStatus } from "@prisma/client";
+import { Order } from "@prisma/client";
 import { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { z } from "zod";
+import { methodNotAllowed } from "~/helpers/app-helpers.server";
 import { parseBody } from "~/lib/parse-body.server";
-import {
-  createOrder,
-  deleteOrder,
-  findOrders,
-  updateOrder,
-} from "~/models/order/order.server";
+import { createOrder, findOrders } from "~/models/order/order.server";
 
 type LoaderData = Order[];
 
@@ -16,27 +12,14 @@ export const loader: LoaderFunction = async (): Promise<LoaderData> => {
 };
 
 export const action: ActionFunction = async ({ request }) => {
-  switch (request.method.toLowerCase()) {
-    case "post":
-      postRequest(request);
-      break;
-    case "patch":
-      patchRequest(request);
-      break;
-    case "delete":
-      deleteRequest(request);
-      break;
-
-    default: {
-      throw new Response(null, {
-        status: 405,
-        statusText: "Invalid Method",
-      });
-    }
+  if (request.method.toLowerCase() == "post") {
+    handlePOSTRequest(request);
+  } else {
+    methodNotAllowed();
   }
 };
 
-const postRequest = async (request: Request) => {
+const handlePOSTRequest = async (request: Request) => {
   const schema = z.object({
     customerId: z.string().cuid(),
   });
@@ -47,61 +30,6 @@ const postRequest = async (request: Request) => {
     const data = parsedData.data;
     const createdOrder = await createOrder(data.customerId);
     throw new Response(JSON.stringify({ createdOrder }), {
-      status: 200,
-      statusText: "OK",
-    });
-  } else {
-    throw new Response(null, {
-      status: 400,
-      statusText: "Bad Request",
-    });
-  }
-};
-
-const patchRequest = async (request: Request) => {
-  const schema = z.object({
-    id: z.string().cuid(),
-    status: z.nativeEnum(OrderStatus),
-    orderedAt: z.date(),
-    shippedAt: z.nullable(z.date()),
-    deliveredAt: z.nullable(z.date()),
-    customerId: z.string().cuid(),
-  });
-
-  const parsedData = await parseBody(request, schema);
-
-  if (parsedData.success) {
-    const data = parsedData.data;
-    const updatedOrder = await updateOrder(data.id, {
-      status: data.status,
-      orderedAt: data.orderedAt,
-      shippedAt: data.shippedAt,
-      deliveredAt: data.deliveredAt,
-      customerId: data.customerId,
-    });
-    throw new Response(JSON.stringify(updateOrder), {
-      status: 200,
-      statusText: "OK",
-    });
-  } else {
-    throw new Response(null, {
-      status: 400,
-      statusText: "Bad Request",
-    });
-  }
-};
-
-const deleteRequest = async (request: Request) => {
-  const schema = z.object({
-    id: z.string().cuid(),
-  });
-
-  const parsedData = await parseBody(request, schema);
-
-  if (parsedData.success) {
-    const data = parsedData.data;
-    const deletedOrder = await deleteOrder(data.id);
-    throw new Response(JSON.stringify(deletedOrder), {
       status: 200,
       statusText: "OK",
     });
