6776713ddf83391d7e76a34b4b073aa4e3cb4f8c
DaruZero <matteo@danelon.me>
2022-07-06 15:19:14 +0200

    feat(jwt): add jwt verification to some routes

--- app/routes/api/v1/warehouse/activeProductInstances/$activeProductInstaceId.ts
+++ app/routes/api/v1/warehouse/activeProductInstances/$activeProductInstaceId.ts
@@ -2,16 +2,24 @@ import type { ActiveProductInstance } from "@prisma/client";
 import type { LoaderFunction } from "@remix-run/node";
 import {
   badRequestResponse,
+  forbiddenResponse,
   notFoundResponse,
 } from "~/helpers/response-helpers.server";
+import { verifyRequest } from "~/lib/verify-request.server";
 import { findActiveProductInstance } from "~/models/activeProductInstance/activeProductInstance.server";
 
 type LoaderData = ActiveProductInstance;
 
 export const loader: LoaderFunction = async ({
+  request,
   params,
 }): Promise<LoaderData> => {
   const activeProductInstanceId = params.productInstanceId;
+
+  let jwtContent = verifyRequest<"employee">(request);
+  if (jwtContent.role !== "ADMIN") {
+    throw forbiddenResponse();
+  }
 
   if (activeProductInstanceId == null) {
     throw badRequestResponse();
