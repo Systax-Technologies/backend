f1fb788f287cd0f490a29abaff98625194f2b0a4
Denis Petrin <denis.petrin@yoroi.company>
2022-07-07 11:33:15 +0200

    feat(api): improve create order api and move it to orders/index

--- app/routes/api/v1/warehouse/orders/index.ts
+++ app/routes/api/v1/warehouse/orders/index.ts
@@ -1,6 +1,18 @@
 import { Order } from "@prisma/client";
-import type { LoaderFunction } from "@remix-run/node";
-import { findOrders } from "~/models/order/order.server";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { z } from "zod";
+import {
+  badRequestResponse,
+  methodNotAllowedResponse,
+  notFoundResponse,
+  okResponse,
+} from "~/helpers/response-helpers.server";
+import { parseBody } from "~/lib/parse-body.server";
+import { createOrder, findOrders } from "~/models/order/order.server";
+import {
+  findManyProductInstancesAvailableToOrder,
+  updateManyProductInstanceOrders,
+} from "~/models/productInstance/productInstance.server";
 
 type LoaderData = { orders: Order[] };
 
@@ -9,4 +21,72 @@ export const loader: LoaderFunction = async (): Promise<LoaderData> => {
   return {
     orders,
   };
+};
+
+export const action: ActionFunction = async ({
+  request,
+}): Promise<Response> => {
+  switch (request.method.toLowerCase()) {
+    case "post": {
+      return handlePOSTRequest(request);
+    }
+
+    default: {
+      return methodNotAllowedResponse();
+    }
+  }
+};
+
+const handlePOSTRequest = async (request: Request): Promise<Response> => {
+  const schema = z.object({
+    customerId: z.string().cuid(),
+    products: z
+      .object({
+        productId: z.string().cuid(),
+        quantity: z.number().int().positive(),
+      })
+      .array(),
+  });
+
+  const parsedData = await parseBody(request, schema);
+
+  /**
+   * Query to find the `ProductInstance`s necessary for the `Order`
+   */
+  let productInstancesIds: string[] = [];
+  for (var product of parsedData.products) {
+    const queryResult = await findManyProductInstancesAvailableToOrder(
+      product.productId,
+      product.quantity
+    );
+
+    if (!queryResult) {
+      return notFoundResponse();
+    }
+
+    productInstancesIds = [...productInstancesIds, ...queryResult];
+  }
+
+  /**
+   * Query to create the `Order`
+   */
+  const createdOrder = await createOrder({ customerId: parsedData.customerId });
+
+  if (!createdOrder) {
+    return badRequestResponse();
+  }
+
+  /**
+   * Query to update the `ProductInstance`s using the array of `id`s previously got
+   */
+  const numberOfUpdatedProductInstances = await updateManyProductInstanceOrders(
+    productInstancesIds,
+    createdOrder.id
+  );
+
+  if (numberOfUpdatedProductInstances.count != productInstancesIds.length) {
+    return badRequestResponse();
+  }
+
+  return okResponse(JSON.stringify(createdOrder));
 };
