df7f6df13221715477b690d43889cf7f38220e71
DaruZero <matteo@danelon.me>
2022-07-10 15:10:51 +0200

    refactor(api): moved PATCH and DELETE endpoints for productInstance to parametric endpoint

--- app/routes/api/v1/warehouse/productInstances/$productInstanceId.ts
+++ app/routes/api/v1/warehouse/productInstances/$productInstanceId.ts
@@ -1,11 +1,21 @@
 import type { ProductInstance } from "@prisma/client";
-import type { LoaderFunction } from "@remix-run/node";
+import { ProductInstanceStatus } from "@prisma/client";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { z } from "zod";
 import {
   badRequestResponse,
+  forbiddenResponse,
+  methodNotAllowedResponse,
   notFoundResponse,
+  okResponse,
 } from "~/helpers/response-helpers.server";
+import { parseBody } from "~/lib/parse-body.server";
 import { verifyEmployeeRequest } from "~/lib/verify-request.server";
-import { findProductInstance } from "~/models/productInstance/productInstance.server";
+import {
+  deleteProductInstance,
+  findProductInstance,
+  updateProductInstance,
+} from "~/models/productInstance/productInstance.server";
 
 type LoaderData = ProductInstance;
 
@@ -28,4 +38,70 @@ export const loader: LoaderFunction = async ({
   }
 
   return productInstance;
+};
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const productInstanceId = params.productInstanceId;
+
+  if (productInstanceId == null) {
+    throw badRequestResponse();
+  }
+
+  switch (request.method.toLowerCase()) {
+    case "patch": {
+      return handlePATCHRequest(request, productInstanceId);
+    }
+    case "delete": {
+      return handleDELETERequest(request, productInstanceId);
+    }
+    default: {
+      return methodNotAllowedResponse();
+    }
+  }
+};
+const handlePATCHRequest = async (
+  request: Request,
+  productInstanceId: string,
+): Promise<Response> => {
+  const jwtContent = await verifyEmployeeRequest(request);
+  if (jwtContent.role !== "ADMIN") {
+    throw forbiddenResponse();
+  }
+  const schema = z.object({
+    status: z.nativeEnum(ProductInstanceStatus),
+    orderId: z.nullable(z.string().cuid()),
+    productId: z.string().cuid(),
+  });
+
+  const data = await parseBody(request, schema);
+
+  const updatedProductInstance = await updateProductInstance(
+    productInstanceId,
+    {
+      status: data.status,
+      orderId: data.orderId,
+      productId: data.productId,
+    },
+  );
+
+  if (!updatedProductInstance) {
+    return notFoundResponse();
+  }
+
+  return okResponse(JSON.stringify(updatedProductInstance));
+};
+
+const handleDELETERequest = async (
+  request: Request,
+  productInstanceId: string,
+): Promise<Response> => {
+  await verifyEmployeeRequest(request);
+
+  const deletedProductInstance = await deleteProductInstance(productInstanceId);
+
+  if (deletedProductInstance == null) {
+    return notFoundResponse();
+  }
+
+  throw okResponse(JSON.stringify(deletedProductInstance));
 };
--- app/routes/api/v1/warehouse/productInstances/index.ts
+++ app/routes/api/v1/warehouse/productInstances/index.ts
@@ -1,10 +1,8 @@
-import { ProductInstanceStatus } from "@prisma/client";
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { z } from "zod";
 import {
   badRequestResponse,
-  forbiddenResponse,
-  notFoundResponse,
+  methodNotAllowedResponse,
   okResponse,
 } from "~/helpers/response-helpers.server";
 import { parseBody } from "~/lib/parse-body.server";
@@ -12,9 +10,7 @@ import { verifyEmployeeRequest } from "~/lib/verify-request.server";
 import type { ProductInstances } from "~/models/dto";
 import {
   createManyProductInstances,
-  deleteProductInstance,
   findManyProductInstances,
-  updateProductInstance,
 } from "~/models/productInstance/productInstance.server";
 
 type LoaderData = {
@@ -34,25 +30,13 @@ export const loader: LoaderFunction = async ({
 export const action: ActionFunction = async ({
   request,
 }): Promise<Response> => {
-  const map: Record<string, (request: Request) => Promise<Response>> = {
-    post: handlePOSTRequest,
-    patch: handlePATCHRequest,
-    delete: handleDELETERequest,
-  };
-
-  const method = request.method.toLowerCase();
-
-  if (method in map) {
-    let jwtContent = await verifyEmployeeRequest(request);
-    if (method !== "patch") {
-      if (jwtContent.role !== "ADMIN") {
-        throw forbiddenResponse();
-      }
-    }
-    return map[method](request);
+  if (request.method.toLowerCase() !== "post") {
+    return methodNotAllowedResponse();
   }
 
-  return badRequestResponse();
+  await verifyEmployeeRequest(request);
+
+  return handlePOSTRequest(request);
 };
 
 const handlePOSTRequest = async (request: Request): Promise<Response> => {
@@ -71,42 +55,4 @@ const handlePOSTRequest = async (request: Request): Promise<Response> => {
   return okResponse(
     JSON.stringify({ numberOfCreatedProducts: createdProductInstances }),
   );
-};
-
-const handlePATCHRequest = async (request: Request): Promise<Response> => {
-  const schema = z.object({
-    id: z.string().cuid(),
-    status: z.nativeEnum(ProductInstanceStatus),
-    orderId: z.nullable(z.string().cuid()),
-    productId: z.string().cuid(),
-  });
-
-  const data = await parseBody(request, schema);
-
-  const updatedProductInstance = await updateProductInstance(data.id, {
-    status: data.status,
-    orderId: data.orderId,
-    productId: data.productId,
-  });
-
-  if (!updatedProductInstance) {
-    return notFoundResponse();
-  }
-
-  return okResponse(JSON.stringify(updatedProductInstance));
-};
-
-const handleDELETERequest = async (request: Request): Promise<Response> => {
-  const schema = z.object({
-    id: z.string().cuid(),
-  });
-
-  const data = await parseBody(request, schema);
-  const deletedProductInstance = await deleteProductInstance(data.id);
-
-  if (deletedProductInstance == null) {
-    return notFoundResponse();
-  }
-
-  throw okResponse(JSON.stringify(deletedProductInstance));
 };
